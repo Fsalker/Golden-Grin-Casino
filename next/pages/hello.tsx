e4961510e6cbde90f6fe8323fa33153a4a9af241
Fsalker <Andreiii500@yahoo.com>
2022-02-09 22:43:14 +0200

    feat: add 3 new font families

--- next/pages/hello.tsx
+++ next/pages/hello.tsx
@@ -1,7 +1,18 @@
 import { FunctionComponent } from 'react';
 
 const Hello: FunctionComponent = () => {
-  return <div className="text-8xl font-bold underline">Hey there</div>;
+  return (
+    <>
+      <div className="font-alfaSlabOne text-8xl underline">HEY THERE</div>
+      <div className="font-courierPrime text-8xl underline">HEY THERE</div>
+      <div className="font-courierPrimeBold text-8xl underline">HEY THERE</div>
+      <div className="font-wingdings text-8xl underline">HEY THERE</div>
+      <div className="font-alfa text-8xl underline">HEY THERE</div>
+      <div className="font-sans text-8xl underline">HEY THERE</div>
+      <div className="font-serif text-8xl underline">HEY THERE</div>
+      <div className="font-mono text-8xl underline">HEY THERE</div>
+    </>
+  );
 };
 
 export default Hello;
--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      wingdings: 'Wingdings',
+      alfaSlabOne: 'AlfaSlabOne',
+      courierPrime: 'CourierPrime',
+      courierPrimeBold: 'CourierPrimeBold',
+    },
     extend: {},
   },
   plugins: [],

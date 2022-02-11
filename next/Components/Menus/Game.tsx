73b1964939c238ad5fe0db9079df84f9f5259a96
Fsalker <Andreiii500@yahoo.com>
2022-02-11 19:54:40 +0200

    feat: add WIN and LOSE banner/text (the game is fully playable now)

--- next/Components/Menus/Game.tsx
+++ next/Components/Menus/Game.tsx
@@ -6,11 +6,30 @@ import DrawnCards from '../Cards/DrawnCards';
 import AcesLeftCounter from '../Counters/AcesLeftCounter';
 import CardsLeftCounter from '../Counters/CardsLeftCounter';
 import { useRecoilState } from 'recoil';
-import { cardsLeftState, gameState } from '../../recoil/atoms';
+import { gameState } from '../../recoil/atoms';
+import asd from '../../public/gameWonBannerDesktop.svg';
 
 const Game: FunctionComponent = () => {
-  const [cardsLeft] = useRecoilState(cardsLeftState);
   const [gameStatus] = useRecoilState(gameState);
+
+  const winBanner =
+    gameStatus === 'won' ? (
+      <div className="flex justify-center mt-[-46px] mb-[-24px]">
+        <img src={asd.src} />
+      </div>
+    ) : (
+      ''
+    );
+
+  const loseText = (
+    <div
+      className="flex flex-col items-center font-courierPrime text-[36px] text-white"
+      style={{ visibility: gameStatus === 'lost' ? 'visible' : 'hidden' }}
+    >
+      <div>You lose.</div>
+      <div>Better luck next time!</div>
+    </div>
+  );
 
   const unfinishedGameButtons = (
     <>
@@ -23,26 +42,28 @@ const Game: FunctionComponent = () => {
   );
 
   const finishedGameButtons = (
-    <>
+    <div className="mt-[-66px]">
+      {loseText}
       <div className="flex flex-col items-center mx-16 mt-4">
         <Reset text={'Play Again'} />
       </div>
       <div className="flex items-left mx-16 mt-4">
         <LeaveGame />
       </div>
-    </>
+    </div>
   );
 
   const gameButtons = gameStatus === 'in progress' ? unfinishedGameButtons : finishedGameButtons;
 
   return (
     <div className="h-full">
-      <div className="h-3/4">
+      <div className="h-[70%]">
         <div className="flex justify-center">
           <CardsLeftCounter />
           <div className="mx-4" />
           <AcesLeftCounter />
         </div>
+        {winBanner}
         <div className="mt-24">
           <DrawnCards />
         </div>

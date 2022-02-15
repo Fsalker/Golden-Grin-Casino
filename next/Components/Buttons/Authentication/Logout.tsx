20081368dd5ed02a2b6580c0cbfe7eeae9de77de
Fsalker <Andreiii500@yahoo.com>
2022-02-15 08:56:37 +0200

    fix: reset apollo cache after logging out

--- next/Components/Buttons/Authentication/Logout.tsx
+++ next/Components/Buttons/Authentication/Logout.tsx
@@ -2,13 +2,15 @@ import { FunctionComponent } from 'react';
 import SmallButton from '../Wrappers/SmallButton';
 import { useRecoilState } from 'recoil';
 import { loggedInState } from '../../../recoil/atoms';
+import apolloClient from '../../../utils/apolloClient';
 
 const Logout: FunctionComponent = () => {
   const [, setLoggedIn] = useRecoilState(loggedInState);
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     localStorage.removeItem('jwt');
     setLoggedIn(false);
+    await apolloClient.resetStore();
   };
 
   return (

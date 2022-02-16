9b1272b96b4bf8890986f1a5826bf366a977528b
Fsalker <Andreiii500@yahoo.com>
2022-02-16 03:22:50 +0200

    feat: add slider debounce timer & show Pacman loader (aka don't POST 50 times/second)

--- next/Components/Menus/MainMenu/UserStats.tsx
+++ next/Components/Menus/MainMenu/UserStats.tsx
@@ -1,23 +1,36 @@
 import { FunctionComponent, useEffect, useState } from 'react';
 import accountHistoryRequest from '../../gql-requests/accountHistory';
+import ConfettiLeft from '../../../public/loading.svg';
 
 const UserStats: FunctionComponent = () => {
   const [spanMinutes, setSpanMinutes] = useState(5);
   const [numGames, setNumGames] = useState(null as null | number);
   const [streak, setStreak] = useState(null as null | number);
+  const [showLoader, setShowLoader] = useState(false);
 
-  useEffect(() => {
+  const getAccountHistory = async () => {
+    // TODO: Add a debounce timer to the input slider
+    const { data } = await accountHistoryRequest({ spanMinutes });
     console.log('Fetching...');
 
-    const getAccountHistory = async () => {
-      // TODO: Add a debounce timer to the input slider
-      const { data } = await accountHistoryRequest({ spanMinutes });
+    setNumGames(data.accountHistory.gamesPlayed);
+    setStreak(data.accountHistory.winningStreak || -data.accountHistory.losingStreak);
+    setShowLoader(false);
+  };
 
-      setNumGames(data.accountHistory.gamesPlayed);
-      setStreak(data.accountHistory.winningStreak || -data.accountHistory.losingStreak);
-    };
+  useEffect(() => {
+    if (!showLoader) {
+      setShowLoader(true);
+    }
 
-    getAccountHistory();
+    let timeoutAccountHistory: ReturnType<typeof setTimeout>;
+    if (numGames === null) {
+      getAccountHistory();
+    } else {
+      timeoutAccountHistory = setTimeout(getAccountHistory, 1000);
+    }
+
+    return () => timeoutAccountHistory && clearTimeout(timeoutAccountHistory);
   }, [spanMinutes]);
 
   const streakText = !streak ? (
@@ -32,17 +45,20 @@ const UserStats: FunctionComponent = () => {
     </div>
   );
 
+  const loader = showLoader && <img src={ConfettiLeft.src} className="inline w-8" />;
+
   return (
     <div className="flex flex-col items-center w-[400px] mt-4">
       <div className="text-gray-300">
+        {loader}
         You have played {numGames} games in the last {spanMinutes} minutes.
       </div>
       <input
         className="w-full"
         type="range"
-        min="10"
+        min="1"
         max="60"
-        step={'5'}
+        step={'1'}
         value={spanMinutes}
         onChange={(e) => setSpanMinutes(parseInt(e.target.value))}
       />

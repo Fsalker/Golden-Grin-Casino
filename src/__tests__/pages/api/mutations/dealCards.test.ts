1d3c56c8223304928e68c851546e216102388864
Fsalker <Andreiii500@yahoo.com>
2022-02-19 11:52:31 +0200

    fix: mock game.update() && refactor auth test && remove unused import

--- src/__tests__/pages/api/mutations/dealCards.test.ts
+++ src/__tests__/pages/api/mutations/dealCards.test.ts
@@ -2,6 +2,7 @@ import { mutationDealCards } from "../../../../components/gql-requests/gql-queri
 import { jwtInvalidErrorMessage } from "../../../../pages/api/gql-modules/types";
 import { prismaMock } from "../../../../../prisma/prismaClientMocked";
 import { ApolloServerBase } from "apollo-server-core";
+import prisma from "../../../../../prisma/prismaClient";
 
 let testApolloServer: ApolloServerBase,
   testAuthenticatedApolloServer: ApolloServerBase;
@@ -17,10 +18,7 @@ it("Should throw auth error when userId doesn't exist in the context", async () 
   const { errors } = await testApolloServer.executeOperation({
     query: mutationDealCards,
   });
-
-  expect(errors).toBeTruthy();
-  expect(errors!.length).toEqual(1);
-  expect(errors![0].message).toEqual(jwtInvalidErrorMessage);
+  expect(errors?.[0]?.message).toEqual(jwtInvalidErrorMessage);
 });
 
 it("Should return 'null' when we cannot deal cards", async () => {
@@ -32,8 +30,9 @@ it("Should return 'null' when we cannot deal cards", async () => {
   expect(data).toEqual({ dealCards: null });
 });
 
-it.only("Should return an array of cards", async () => {
+it("Should return an array of cards", async () => {
   prismaMock.game.findMany.mockReturnValue([mockedNewGame]);
+  prismaMock.game.update.mockReset();
   const { data } = await testAuthenticatedApolloServer.executeOperation({
     query: mutationDealCards,
   });
@@ -43,7 +42,7 @@ it.only("Should return an array of cards", async () => {
 });
 
 const mockedNewGame = {
-  id: 208,
+  id: -288,
   createdAt: new Date("2022-02-19T06:08:09.082Z"),
   userId: -888,
   deck: [10, 3, 0, 2, 8, 9, 4, 5, 7, 11, 6, 1],
